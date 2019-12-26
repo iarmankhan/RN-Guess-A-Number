de0c6b752544119b22e254a9a6b8b9e3e9333782
iarmankhan <armankhan9244@gmail.com>
2019-12-26 17:03:09 +0530

    Minor Fixes
    -Formatted Files

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import {StyleSheet, View} from 'react-native';
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
-  return (
-    <View style={styles.screen}>
-      <Header title="Guess a Number" />
-      <StartGameScreen />
-    </View>
-  );
+    return (
+        <View style={styles.screen}>
+            <Header title="Guess a Number"/>
+            <StartGameScreen/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1
-  }
+    screen: {
+        flex: 1
+    }
 });
