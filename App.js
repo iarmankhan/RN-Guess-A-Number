7af167c93e20d300cc7df6429a3d9c978353a08d
iarmankhan <armankhan9244@gmail.com>
2019-12-31 19:10:23 +0530

    SafeAreaView Implementation

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, SafeAreaView} from 'react-native';
 
 import * as Font from 'expo-font';
-import { AppLoading } from "expo";
+import {AppLoading} from "expo";
 
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
@@ -22,8 +22,9 @@ export default function App() {
     const [guessRounds, setGuessRounds] = useState(0);
     const [dataLoaded, setDataLoaded] = useState(false);
 
-    if(!dataLoaded) {
-        return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)} onError={err => console.log(err)} />;
+    if (!dataLoaded) {
+        return <AppLoading startAsync={fetchFonts} onFinish={() => setDataLoaded(true)}
+                           onError={err => console.log(err)}/>;
     }
 
     const configureNewGameHandler = () => {
@@ -36,22 +37,22 @@ export default function App() {
     };
 
     const gameOverHandler = numOfRounds => {
-      setGuessRounds(numOfRounds);
+        setGuessRounds(numOfRounds);
     };
 
-    let content = <StartGameScreen onStartGame={startGameHandler} />;
+    let content = <StartGameScreen onStartGame={startGameHandler}/>;
 
-    if(userNumber && guessRounds <= 0){
-        content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
+    if (userNumber && guessRounds <= 0) {
+        content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>;
     } else if (guessRounds > 0) {
-        content = <GameOverScreen rounds={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />
+        content = <GameOverScreen rounds={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
     }
 
     return (
-        <View style={styles.screen}>
+        <SafeAreaView style={styles.screen}>
             <Header title="Guess a Number"/>
             {content}
-        </View>
+        </SafeAreaView>
     );
 }
 
