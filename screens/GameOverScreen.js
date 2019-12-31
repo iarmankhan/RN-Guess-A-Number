8d9ebdc02a24f590754fafab676901bb65829591
iarmankhan <armankhan9244@gmail.com>
2019-12-31 16:39:08 +0530

    Added conditions to change layout WRT device width and height in GameOverScreen

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {View, Text, StyleSheet, ScrollView, Image, Dimensions} from "react-native";
 
 import BodyText from "../components/BodyText";
@@ -8,18 +8,43 @@ import MainButton from "../components/MainButton";
 
 
 const GameOverScreen = props => {
+    const [availableDeviceWidth, setAvailableDeviceWidth] = useState(Dimensions.get('window').width);
+    const [availableDeviceHeight, setAvailableDeviceHeight] = useState(Dimensions.get('window').height);
+
+    useEffect(() => {
+        const updateLayout = () => {
+            setAvailableDeviceHeight(Dimensions.get('window').width);
+            setAvailableDeviceHeight(Dimensions.get('window').height);
+        };
+
+        Dimensions.addEventListener('change', updateLayout);
+
+        return () => {
+            Dimensions.removeEventListener('change', updateLayout);
+       }
+    });
+
     return (
         <ScrollView>
             <View style={styles.screen}>
                 <TitleText style={{fontSize: 23}}>The Game is Over!</TitleText>
-                <View style={styles.imageContainer}>
+                <View style={‌{...styles.imageContainer, ...{
+                    width: availableDeviceWidth * 0.7,
+                    height: availableDeviceWidth * 0.7,
+                    borderRadius: (availableDeviceWidth * 0.7) / 2,
+                    marginVertical: availableDeviceHeight / 30
+                }}}>
                     <Image source={require('../assets/success.png')} style={styles.image}/>
                     {/*<Image source={{uri: 'https://image.freepik.com/free-vector/explorer-with-backpack-background_23-2148159527.jpg'}} style={styles.image}/>*/}
                 </View>
-                <View style={styles.infoContainer}>
-                    <BodyText style={styles.resultText}>
-                        Your phone needed <Text style={styles.highlight}>{props.rounds}</Text> rounds to guess the
-                        number <Text style={styles.highlight}>{props.userNumber}</Text>
+                <View style={‌{...styles.infoContainer,
+                    ...{marginVertical: availableDeviceHeight / 60}
+                }}>
+                    <BodyText style={‌{...styles.resultText, ...{
+                        fontSize: availableDeviceHeight < 400 ? 16 : 20}}}>
+                        Your phone needed{' '}
+                        <Text style={styles.highlight}>{props.roundsNumber}</Text> rounds to guess the number{' '}
+                        <Text style={styles.highlight}>{props.userNumber}</Text>
                     </BodyText>
                     <View style={styles.buttonContainer}>
                         <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
@@ -38,13 +63,9 @@ const styles = StyleSheet.create({
         paddingVertical: 10
     },
     imageContainer: {
-        width: Dimensions.get('window').width * 0.7,
-        height: Dimensions.get('window').width * 0.7,
-        borderRadius: (Dimensions.get('window').width * 0.7) / 2,
         borderWidth: 3,
         borderColor: 'black',
         overflow: 'hidden',
-        marginVertical: Dimensions.get('window').height / 20
     },
     image: {
         width: '100%',
@@ -52,13 +73,11 @@ const styles = StyleSheet.create({
     },
     infoContainer: {
         marginHorizontal: 30,
-        marginVertical: Dimensions.get('window').height / 40,
         alignItems: 'center',
         justifyContent: 'center'
     },
     resultText: {
         textAlign: 'center',
-        fontSize: Dimensions.get('window').height < 400 ? 16 : 20
     },
     highlight: {
         color: Colors.primary,
